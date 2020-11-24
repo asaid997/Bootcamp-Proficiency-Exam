28e7474813cb10a2215145b9c9cdde4d18c6745e
HealthyJunkie <androwas101@gmail.com>
2020-11-24 10:50:47 +0200

    first commit

---
+++ dist/server-manager.js
@@ -0,0 +1,5 @@
+class ServerManager {
+    constructor() {
+    }
+
+}
---
+++ server/routes/api.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const urllib = require('urllib')
+const router = express.Router()
+
+
+module.exports = router
